832efe56b361114b4084553cc5726520884055f4
Dan Strong <danielstrong@mac.com>
2024-08-25 19:12:00 -0800

    add total paid and logic to add to it

--- final/src/assets/script.js
+++ final/src/assets/script.js
@@ -1,6 +1,4 @@
-
-
-/* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
+let totalPaid = 0;
 let products = [];
 
 const cherry = { 
@@ -31,14 +29,8 @@ products.push(cherry);
 products.push(orange);
 products.push(strawberry);
 
-/* Declare an empty array named cart to hold the items in the cart */
 let cart = [];
 
-/* Create a function named addProductToCart that takes in the product productId as an argument
-  - addProductToCart should get the correct product based on the productId
-  - addProductToCart should then increase the product's quantity
-  - if the product is not already in the cart, add it to the cart
-*/
 const addProductToCart = (productId) => {
   for (product of products){
     if (product.productId === productId){
@@ -52,10 +44,6 @@ const addProductToCart = (productId) => {
   }
 }
 
-/* Create a function named increaseQuantity that takes in the productId as an argument
-  - increaseQuantity should get the correct product based on the productId
-  - increaseQuantity should then increase the product's quantity
-*/
 const increaseQuantity = (productId) => {
   for (product of products){
     if (product.productId === productId){
@@ -63,12 +51,6 @@ const increaseQuantity = (productId) => {
     }
   }
 }
-
-/* Create a function named decreaseQuantity that takes in the productId as an argument
-  - decreaseQuantity should get the correct product based on the productId
-  - decreaseQuantity should decrease the quantity of the product
-  - if the function decreases the quantity to 0, the product is removed from the cart
-*/
 
 const decreaseQuantity = (productId) => {
   for (const product of cart){
@@ -83,11 +65,6 @@ const decreaseQuantity = (productId) => {
   }
 }
 
-/* Create a function named removeProductFromCart that takes in the productId as an argument
-  - removeProductFromCart should get the correct product based on the productId
-  - removeProductFromCart should update the product quantity to 0
-  - removeProductFromCart should remove the product from the cart
-*/
 const removeProductFromCart = (productId) => {
   for (const product of cart){
     if (product.productId === productId){
@@ -97,11 +74,6 @@ const removeProductFromCart = (productId) => {
   }
 }
 
-/* Create a function named cartTotal that has no parameters
-  - cartTotal should iterate through the cart to get the total cost of all products
-  - cartTotal should return the total cost of the products in the cart
-  Hint: price and quantity can be used to determine total cost
-*/
 const cartTotal = () => {
   let total = 0;
 
@@ -112,30 +84,15 @@ const cartTotal = () => {
   return total;
 }
 
-/* Create a function called emptyCart that empties the products from the cart */
 const emptyCart = () => {
   cart = [];
 }
 
-/* Create a function named pay that takes in an amount as an argument
-  - amount is the money paid by customer
-  - pay will return a negative number if there is a remaining balance
-  - pay will return a positive number if money should be returned to customer
-  Hint: cartTotal function gives us cost of all the products in the cart  
-*/
 const pay = (amount) => {
   const balance = amount - cartTotal();
+  totalPaid += balance;
   return Number(balance.toFixed(2));
 }
-
-/* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
-
-
-/* The following is for running unit tests. 
-   To fully complete this project, it is expected that all tests pass.
-   Run the following command in terminal to run tests
-   npm run test
-*/
 
 module.exports = {
    products,
